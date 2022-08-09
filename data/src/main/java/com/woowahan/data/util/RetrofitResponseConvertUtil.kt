5b8a9559cd3c23aa15de9499bafab1e00936a092
wjchoi96 <wjchoi96@naver.com>
2022-08-09 23:47:09 +0900

    [Feature] #4 StatusCode 가 없는 Api 요청을 위해 StatusCode 가 없는 getData 메소드를 오버로딩하여 추가

--- data/src/main/java/com/woowahan/data/util/RetrofitResponseConvertUtil.kt
+++ data/src/main/java/com/woowahan/data/util/RetrofitResponseConvertUtil.kt
@@ -8,6 +8,23 @@ object RetrofitResponseConvertUtil {
         return getDataOrError(response, statusCode).body()!!
     }
 
+    fun <T>getData(response: Response<T>): T {
+        return getDataOrError(response).body()!!
+    }
+
+    private fun <T>getDataOrError(response: Response<T>): Response<T> {
+        val errorMessage: String? = when {
+            response.body() == null -> "api response body is null"
+            !response.isSuccessful -> response.errorBody()?.string() ?: response.message()
+            else -> null
+        }
+        return if(errorMessage == null){
+            response
+        }else{
+            throw Throwable(errorMessage)
+        }
+    }
+
     private fun <T>getDataOrError(response: Response<T>, statusCode: Int?): Response<T> {
         val errorMessage: String? = when {
             statusCode == null || statusCode != 200 -> "api response status code is not 200[$statusCode]"
