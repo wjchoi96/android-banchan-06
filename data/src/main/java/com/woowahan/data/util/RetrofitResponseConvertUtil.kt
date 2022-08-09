2a03b5b2de2c10d6ac045814655c4a5b20d43264
wjchoi96 <wjchoi96@naver.com>
2022-08-09 23:36:46 +0900

    [Feature] #4 Data 모듈에 Retrofit Response Convert Util 클래스 추가
     - Api Service 들의 리턴값을 래핑한 Response의 성공 여부를 판단하여 그에 맞는 리턴값으로 변환

---
+++ data/src/main/java/com/woowahan/data/util/RetrofitResponseConvertUtil.kt
@@ -0,0 +1,24 @@
+package com.woowahan.data.util
+
+import retrofit2.Response
+
+object RetrofitResponseConvertUtil {
+
+    fun <T>getData(response: Response<T>, statusCode: Int?): T {
+        return getDataOrError(response, statusCode).body()!!
+    }
+
+    private fun <T>getDataOrError(response: Response<T>, statusCode: Int?): Response<T> {
+        val errorMessage: String? = when {
+            statusCode == null || statusCode != 200 -> "api response status code is not 200[$statusCode]"
+            response.body() == null -> "api response body is null"
+            !response.isSuccessful -> response.errorBody()?.string() ?: response.message()
+            else -> null
+        }
+        return if(errorMessage == null){
+            response
+        }else{
+            throw Throwable(errorMessage)
+        }
+    }
+}
