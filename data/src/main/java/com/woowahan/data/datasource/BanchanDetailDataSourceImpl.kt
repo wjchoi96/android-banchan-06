683664bff19654351c54eee2a55f4961108d56d2
wjchoi96 <wjchoi96@naver.com>
2022-08-09 23:47:59 +0900

    [Feature] Banchan Detail DataSource, 그 구현체 클래스 추가

---
+++ data/src/main/java/com/woowahan/data/datasource/BanchanDetailDataSourceImpl.kt
@@ -0,0 +1,17 @@
+package com.woowahan.data.datasource
+
+import com.woowahan.data.apiservice.BanchanDetailApiService
+import com.woowahan.data.entity.BanchanDetailEntity
+import com.woowahan.data.util.RetrofitResponseConvertUtil
+import javax.inject.Inject
+
+class BanchanDetailDataSourceImpl @Inject constructor(
+    private val banchanDetailApiService: BanchanDetailApiService
+): BanchanDetailDataSource {
+
+    override suspend fun fetchBanchanDetail(banchanHash: String): BanchanDetailEntity {
+         banchanDetailApiService.fetchBanchanDetail(banchanHash).let {
+             return RetrofitResponseConvertUtil.getData(it)
+         }
+    }
+}
