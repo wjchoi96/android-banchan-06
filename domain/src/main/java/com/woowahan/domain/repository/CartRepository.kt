710f3ad0766b2e46569026c685c2370b5ba73bb5
wjchoi96 <wjchoi96@naver.com>
2022-08-11 00:49:51 +0900

    [Feature] #20 Cart Repository, 구현체 추가

---
+++ domain/src/main/java/com/woowahan/domain/repository/CartRepository.kt
@@ -0,0 +1,13 @@
+package com.woowahan.domain.repository
+
+import com.woowahan.domain.model.BanchanModel
+
+interface CartRepository {
+    suspend fun insertCartItem(banchan: BanchanModel, count: Int): Result<Boolean>
+
+    suspend fun removeCartItem(hash: String): Result<Boolean>
+
+    suspend fun removeCartItems(hashes: List<String>): Result<Boolean>
+
+    suspend fun updateCartItem(hash: String, count: Int): Result<Boolean>
+}
