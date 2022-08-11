4876af5e9abf3594d75e84a66479588356060d34
wjchoi96 <wjchoi96@naver.com>
2022-08-12 01:30:59 +0900

    [Feature] #22 Fetch Banchan UseCase 들에 FetchCartItemsUseCase 의존성 추가 후 isCartItem 속성을 적용하도록 수정

--- domain/src/main/java/com/woowahan/domain/usecase/FetchSoupDishBanchanUseCase.kt
+++ domain/src/main/java/com/woowahan/domain/usecase/FetchSoupDishBanchanUseCase.kt
@@ -4,14 +4,21 @@ import com.woowahan.domain.model.BanchanModel
 import com.woowahan.domain.repository.BanchanRepository
 
 class FetchSoupDishBanchanUseCase(
-    private val banchanRepository: BanchanRepository
+    private val banchanRepository: BanchanRepository,
+    private val fetchCartItemsUseCase: FetchCartItemsUseCase
 ) {
     suspend operator fun invoke(): Result<List<BanchanModel>>{
         return kotlin.runCatching {
+            val cart = fetchCartItemsUseCase().getOrThrow()
             listOf(
                 BanchanModel.empty().copy(viewType = BanchanModel.ViewType.Banner),
                 BanchanModel.empty().copy(viewType = BanchanModel.ViewType.Header),
-            ) + banchanRepository.fetchSoupDishBanchan().getOrThrow()
+            ) + banchanRepository.fetchSoupDishBanchan().getOrThrow().map {
+                if(cart[it.hash] != null)
+                    it.copy(isCartItem = true)
+                else
+                    it
+            }
         }
     }
 }
--- presentation/src/main/java/com/woowahan/banchan/di/UseCaseModule.kt
+++ presentation/src/main/java/com/woowahan/banchan/di/UseCaseModule.kt
@@ -1,6 +1,5 @@
 package com.woowahan.banchan.di
 
-import com.woowahan.data.repository.CartRepositoryImpl
 import com.woowahan.domain.repository.BanchanRepository
 import com.woowahan.domain.repository.CartRepository
 import com.woowahan.domain.usecase.*
@@ -14,8 +13,10 @@ import dagger.hilt.android.components.ViewModelComponent
 object UseCaseModule {
 
     @Provides
-    fun provideFetchBestDishBanchanUseCase(repo: BanchanRepository) =
-        FetchBestBanchanUseCase(repo)
+    fun provideFetchBestDishBanchanUseCase(
+        repo: BanchanRepository,
+        fetchCartItemsUseCase: FetchCartItemsUseCase
+    ) = FetchBestBanchanUseCase(repo, fetchCartItemsUseCase)
 
     @Provides
     fun provideFetchMainDishBanchanUseCase(
@@ -24,12 +25,16 @@ object UseCaseModule {
     ) = FetchMainDishBanchanUseCase(repo, fetchCartItemsUseCase)
 
     @Provides
-    fun provideFetchSoupDishBanchanUseCase(repo: BanchanRepository) =
-        FetchSoupDishBanchanUseCase(repo)
+    fun provideFetchSoupDishBanchanUseCase(
+        repo: BanchanRepository,
+        fetchCartItemsUseCase: FetchCartItemsUseCase
+    ) = FetchSoupDishBanchanUseCase(repo, fetchCartItemsUseCase)
 
     @Provides
-    fun provideFetchSideDishBanchanUseCase(repo: BanchanRepository) =
-        FetchSideDishBanchanUseCase(repo)
+    fun provideFetchSideDishBanchanUseCase(
+        repo: BanchanRepository,
+        fetchCartItemsUseCase: FetchCartItemsUseCase
+    ) = FetchSideDishBanchanUseCase(repo, fetchCartItemsUseCase)
 
     @Provides
     fun provideFetchCartItemsUseCase(repo: CartRepository) =
