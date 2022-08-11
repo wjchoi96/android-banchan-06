fe90e07d7d110ab7eb82db00843e8911e8eb8c40
youngmu <tyg04308@naver.com>
2022-08-11 17:22:16 +0900

    [Rename] #31 - Rename main component

--- presentation/src/main/java/com/woowahan/banchan/ui/root/RootTabPagerAdapter.kt
+++ presentation/src/main/java/com/woowahan/banchan/ui/root/RootTabPagerAdapter.kt
@@ -1,10 +1,10 @@
-package com.woowahan.banchan.ui.main
+package com.woowahan.banchan.ui.root
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class MainTabPagerAdapter(
+class RootTabPagerAdapter(
     fragmentActivity: FragmentActivity,
     private val fragmentList: List<Pair<String, Fragment>>
 ): FragmentStateAdapter(fragmentActivity) {
