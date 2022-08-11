958c4def74dfe1ff7123bee1b687e07d08799249
wjchoi96 <wjchoi96@naver.com>
2022-08-11 13:12:35 +0900

    [Feature] #22 Cart Item Insert Bottom Sheet 클래스 추가, xml 과 데이터 바인딩 적용

---
+++ presentation/src/main/java/com/woowahan/banchan/ui/dialog/CartItemInsertBottomSheet.kt
@@ -0,0 +1,73 @@
+package com.woowahan.banchan.ui.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.woowahan.banchan.databinding.DialogCartAddBinding
+import com.woowahan.banchan.util.toCashString
+import com.woowahan.domain.model.BanchanModel
+import timber.log.Timber
+
+class CartItemInsertBottomSheet constructor(
+    private val banchan: BanchanModel,
+    val insertListener: (BanchanModel) -> (Unit)
+): BottomSheetDialogFragment() {
+    private var _binding: DialogCartAddBinding? = null
+    private val binding get() = _binding ?: error("Binding not Initialized")
+
+    private var _itemCount: MutableLiveData<Int> = MutableLiveData(1)
+    val itemCount: LiveData<Int> = _itemCount
+
+    private var _cartCost: MutableLiveData<String> = MutableLiveData(banchan.salePrice ?: banchan.price)
+    var cartCost: LiveData<String> = _cartCost
+
+    val countUpListener: (Int) -> (Unit) = {
+        Timber.d("count up event[$it]")
+        _itemCount.value = it+1
+        setCartCost(it+1)
+    }
+    val countDownListener: (Int) -> (Unit) = {
+        Timber.d("count down event[$it]")
+        if(_itemCount.value != 1) {
+            _itemCount.value = it - 1
+            setCartCost(it-1)
+        }
+    }
+    private fun setCartCost(count: Int){
+        _cartCost.value = when(banchan.salePriceRaw){
+            0L -> (banchan.priceRaw * count).toCashString() + "원"
+            else -> (banchan.salePriceRaw * count).toCashString() + "원"
+        }
+    }
+
+    val cancelListener: ()->(Unit) = {
+        Timber.d("cancel event")
+        this.dismiss()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogCartAddBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.banchan = banchan
+        binding.dialog = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
