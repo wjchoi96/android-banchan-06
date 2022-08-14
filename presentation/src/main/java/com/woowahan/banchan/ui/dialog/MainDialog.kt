6a82fca8e6c9503e7ab236e85545f3a5d4dfaa85
wjchoi96 <wjchoi96@naver.com>
2022-08-15 00:30:12 +0900

    [Rename] #42 명확한 네이밍으로 변경

--- presentation/src/main/java/com/woowahan/banchan/ui/dialog/MainDialog.kt
+++ presentation/src/main/java/com/woowahan/banchan/ui/dialog/MainDialog.kt
@@ -13,11 +13,11 @@ class MainDialog: Dialog {
     constructor(context: Context): super(context) { init() }
     constructor(context: Context, themeResId: Int): super(context, themeResId) { init() }
 
-    lateinit var vd : DialogMainBinding
+    lateinit var binding : DialogMainBinding
 
     private fun init(){
-        vd = DialogMainBinding.inflate(LayoutInflater.from(context), null, false)
-        setContentView(vd.root)
+        binding = DialogMainBinding.inflate(LayoutInflater.from(context), null, false)
+        setContentView(binding.root)
         window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         Timber.d("init dialog")
         setDefaultListener()
@@ -25,34 +25,34 @@ class MainDialog: Dialog {
     }
 
     private fun setDefaultVisible(){
-        vd.btnColor.visibility = View.GONE
-        vd.btnBlack.visibility = View.GONE
+        binding.btnColor.visibility = View.GONE
+        binding.btnBlack.visibility = View.GONE
     }
     private fun setDefaultListener(){
-        vd.btnColor.setOnClickListener { this.dismiss() }
-        vd.btnBlack.setOnClickListener { this.dismiss() }
+        binding.btnColor.setOnClickListener { this.dismiss() }
+        binding.btnBlack.setOnClickListener { this.dismiss() }
     }
 
     fun setPositiveBtn(btnText : String? = null, positiveListener : ((View) -> (Unit))? = null){
-        vd.btnColor.visibility = View.VISIBLE
+        binding.btnColor.visibility = View.VISIBLE
         if(!btnText.isNullOrBlank())
-            vd.btnColor.text = btnText
-        vd.btnColor.setOnClickListener {
+            binding.btnColor.text = btnText
+        binding.btnColor.setOnClickListener {
             positiveListener?.invoke(it)
             this.dismiss()
         }
     }
     fun setNegativeBtn(btnText : String? = null, negativeListener : ((View) -> (Unit))? = null){
-        vd.btnBlack.visibility = View.VISIBLE
+        binding.btnBlack.visibility = View.VISIBLE
         if(!btnText.isNullOrBlank())
-            vd.btnBlack.text = btnText
-        vd.btnBlack.setOnClickListener {
+            binding.btnBlack.text = btnText
+        binding.btnBlack.setOnClickListener {
             negativeListener?.invoke(it)
             this.dismiss()
         }
     }
 
     fun setMessage(message: CharSequence?) {
-        vd.tvTitle.text = message
+        binding.tvTitle.text = message
     }
 }
