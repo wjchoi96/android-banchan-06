90f3cef5223c0bd0473633723db0387c40caeb84
wjchoi96 <wjchoi96@naver.com>
2022-08-15 02:39:57 +0900

    [Test] #13 MockWebServer를 사용한 Best Banchan Api Service 테스트 코드 작성

--- data/src/test/java/com/woowahan/data/datasource/BanchansRetrofitDataSourceImplTest.kt
+++ data/src/test/java/com/woowahan/data/datasource/BanchansRetrofitDataSourceImplTest.kt
@@ -1,52 +1,38 @@
 package com.woowahan.data.datasource
 
+import com.google.gson.Gson
 import com.woowahan.data.apiservice.BestBanchanApiService
 import com.woowahan.data.apiservice.MainDishBanchanApiService
 import com.woowahan.data.apiservice.SideDishBanchanApiService
 import com.woowahan.data.apiservice.SoupDishBanchanApiService
+import com.woowahan.data.entity.BestBanchanEntity
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.net.HttpURLConnection
 
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class BanchansRetrofitDataSourceImplTest{
 
-    private lateinit var server: MockWebServer
-//    val successResponse by lazy {
-//        MockResponse().apply {
-//            setResponseCode(HttpURLConnection.HTTP_OK)
-////            val jsonText = readSuccessJson(context)
-////            setBody(jsonText)
-//        }
-//    }
-//
-//    private fun readSuccessJson(context:Context):String{
-//        return fileReader(context,"bestBanchanResponse.json")
-//    }
-//    private fun fileReader(context: Context, fileName:String):String{
-//        // context.classLoader.getResource()로
-//        // json 위치를 참조합니다. (test/resources)
-//        val file = File(context.classLoader.getResource(fileName).file)
-//        return file.bufferedReader().use {
-//            val str = it.readText()
-//            it.close()
-//            str
-//        }
-//    }
+    private lateinit var mockServer: MockWebServer
+    private lateinit var realRetrofit: Retrofit
+    private lateinit var mockRetrofit: Retrofit
+    private val baseUrl = "https://api.codesquad.kr/onban/"
 
-    private lateinit var retrofit: Retrofit
     @Before
     fun initRetrofit(){
-        retrofit = Retrofit.Builder().apply {
-            baseUrl("https://api.codesquad.kr/onban/")
+        realRetrofit = Retrofit.Builder().apply {
+            baseUrl(baseUrl)
             client(OkHttpClient.Builder().apply {
                 addInterceptor(
                     HttpLoggingInterceptor().apply {
@@ -58,7 +44,58 @@ class BanchansRetrofitDataSourceImplTest{
         }.build()
     }
 
+    @Before fun setUpMockServer(){
+        mockServer = MockWebServer()
+        mockServer.start()
+        mockRetrofit = Retrofit.Builder().apply {
+            baseUrl(mockServer.url(""))
+            addConverterFactory(GsonConverterFactory.create())
+        }.build()
+
+    }
+
+    private fun readResponse(fileName: String): String{
+        return File("src/test/java/com/resources/$fileName").readText()
+    }
+
     @Test
+    fun fetchBestBanchans_mockServerSuccessRequest_isNotNullAndEquals() = runTest {
+        //Given
+        val responseJson = readResponse("best_success.json")
+        val response = MockResponse().apply {
+            setResponseCode(HttpURLConnection.HTTP_OK)
+            setBody(responseJson)
+        }
+        mockServer.enqueue(response)
+        val service = mockRetrofit.create(BestBanchanApiService::class.java)
+
+        //When
+        val actualResult = service.fetchBestBanchans().body()
+
+        //Then
+        val expected = Gson().fromJson(responseJson, BestBanchanEntity::class.java)
+        assertThat(actualResult).isNotNull.isEqualTo(expected)
+    }
+
+    @Test
+    fun fetchBestBanchans_mockServerFailRequest_isEquals() = runTest {
+        //Given
+        val responseJson = readResponse("best_fail.json")
+        val response = MockResponse().apply {
+            setResponseCode(HttpURLConnection.HTTP_OK)
+            setBody(responseJson)
+        }
+        mockServer.enqueue(response)
+        val service = mockRetrofit.create(BestBanchanApiService::class.java)
+
+        //When
+        val actualResult = service.fetchBestBanchans().body()
+
+        //Then
+        val expected = Gson().fromJson(responseJson, BestBanchanEntity::class.java)
+        assertThat(actualResult).isEqualTo(expected)
+    }
+
     @Test
     fun fetchBestBanchans_realServerRequest_isNotNull() = runTest {
         //Given
