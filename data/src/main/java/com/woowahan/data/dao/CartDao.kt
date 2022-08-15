24c2cfe410be7bd89d5fc651384a7b95437874a9
wjchoi96 <wjchoi96@naver.com>
2022-08-15 18:30:58 +0900

    [Add] #67 Cart dao 추가
    Co-authored-by: YM-Byun <tyg04308@naver.com>

---
+++ data/src/main/java/com/woowahan/data/dao/CartDao.kt
@@ -0,0 +1,36 @@
+package com.woowahan.data.dao
+
+import androidx.room.*
+import com.woowahan.data.entity.dto.CartDto
+import com.woowahan.data.entity.table.BanchanItemTableEntity
+import com.woowahan.data.entity.table.CartTableEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CartDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertCartItem(cartItem: BanchanItemTableEntity, cart: CartTableEntity)
+
+    @Query("update ${CartTableEntity.TABLE_NAME} set " +
+            "${CartTableEntity.COLUMN_COUNT} = :count " +
+            "where ${CartTableEntity.COLUMN_HASH} = :hash")
+    fun updateCartItem(hash: String, count: Int): Int
+
+    @Query("DELETE FROM ${CartTableEntity.TABLE_NAME} WHERE ${CartTableEntity.COLUMN_HASH} in (:hash)")
+    fun removeCartItem(vararg hash: String): Int
+    @Query("DELETE FROM ${BanchanItemTableEntity.TABLE_NAME} WHERE ${BanchanItemTableEntity.COLUMN_HASH} in (:hash)")
+    fun removeCartItemInfo(vararg hash: String): Int
+
+    @Query("SELECT COUNT(*) FROM ${CartTableEntity.TABLE_NAME}")
+    fun fetchCartItemsCount(): Flow<Int>
+
+    @Transaction
+    @Query("SELECT * FROM ${CartTableEntity.TABLE_NAME}")
+    fun fetchCartItems(): List<CartDto>
+
+    @Transaction
+    @Query("SELECT * FROM ${CartTableEntity.TABLE_NAME}")
+    fun fetchCartItemsFlow(): Flow<List<CartDto>>
+
+}
