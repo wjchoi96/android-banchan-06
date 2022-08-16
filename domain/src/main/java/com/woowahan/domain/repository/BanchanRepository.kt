127414f85d0da3a894a89b72fbb2e24e4c858099
wjchoi96 <wjchoi96@naver.com>
2022-08-16 12:46:41 +0900

    [Refactor] #75 Banchan Repository 의 메소드들을 Flow로 변경

--- domain/src/main/java/com/woowahan/domain/repository/BanchanRepository.kt
+++ domain/src/main/java/com/woowahan/domain/repository/BanchanRepository.kt
@@ -2,15 +2,16 @@ package com.woowahan.domain.repository
 
 import com.woowahan.domain.model.BanchanModel
 import com.woowahan.domain.model.BestBanchanModel
+import kotlinx.coroutines.flow.Flow
 
 interface BanchanRepository {
 
-    suspend fun fetchBestBanchan(): Result<List<BestBanchanModel>>
+    suspend fun fetchBestBanchan(): Flow<Result<List<BestBanchanModel>>>
 
-    suspend fun fetchMainDishBanchan(): Result<List<BanchanModel>>
+    suspend fun fetchMainDishBanchan(): Flow<Result<List<BanchanModel>>>
 
-    suspend fun fetchSoupDishBanchan(): Result<List<BanchanModel>>
+    suspend fun fetchSoupDishBanchan(): Flow<Result<List<BanchanModel>>>
 
-    suspend fun fetchSideDishBanchan(): Result<List<BanchanModel>>
+    suspend fun fetchSideDishBanchan(): Flow<Result<List<BanchanModel>>>
 
 }
