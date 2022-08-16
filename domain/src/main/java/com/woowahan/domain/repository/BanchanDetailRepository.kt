d1d7af3cfda8d513771dd6c135f1d5fa3f594835
wjchoi96 <wjchoi96@naver.com>
2022-08-16 12:49:05 +0900

    [Refactor] #75 Banchan Detail Repository 리턴 타입을 Flow로 변경

--- domain/src/main/java/com/woowahan/domain/repository/BanchanDetailRepository.kt
+++ domain/src/main/java/com/woowahan/domain/repository/BanchanDetailRepository.kt
@@ -1,7 +1,8 @@
 package com.woowahan.domain.repository
 
 import com.woowahan.domain.model.BanchanDetailModel
+import kotlinx.coroutines.flow.Flow
 
 interface BanchanDetailRepository {
-    suspend fun fetchBanchanDetail(hash: String, title: String): Result<BanchanDetailModel>
+    suspend fun fetchBanchanDetail(hash: String, title: String): Flow<Result<BanchanDetailModel>>
 }
