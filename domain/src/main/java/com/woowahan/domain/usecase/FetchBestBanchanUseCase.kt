1afb8343cf569645c55c4c1055006fe1a507e942
wjchoi96 <wjchoi96@naver.com>
2022-08-16 12:48:22 +0900

    [Refactor] #75 Fetch Banchan UseCase 수정
    - 리턴 타입을 Flow로 변경
    - Repository의 리턴타입 변경에 의한 수정

--- domain/src/main/java/com/woowahan/domain/usecase/FetchBestBanchanUseCase.kt
+++ domain/src/main/java/com/woowahan/domain/usecase/FetchBestBanchanUseCase.kt
@@ -2,24 +2,28 @@ package com.woowahan.domain.usecase
 
 import com.woowahan.domain.model.BestBanchanModel
 import com.woowahan.domain.repository.BanchanRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class FetchBestBanchanUseCase(
     private val banchanRepository: BanchanRepository,
     private val fetchCartItemsKeyUseCase: FetchCartItemsKeyUseCase
 ){
-    suspend operator fun invoke(): Result<List<BestBanchanModel>>{
-        return kotlin.runCatching {
-            val cart = fetchCartItemsKeyUseCase().getOrThrow()
-            listOf(
-                BestBanchanModel.empty().copy(viewType = BestBanchanModel.ViewType.Banner)
-            ) + banchanRepository.fetchBestBanchan().getOrThrow().map {
-                it.banchans.map { banchan ->
-                    if(cart.contains(banchan.hash))
-                        banchan.copy(isCartItem = true)
-                    else
-                        banchan
+    suspend operator fun invoke(): Flow<Result<List<BestBanchanModel>>> {
+        return banchanRepository.fetchBestBanchan().map {
+            kotlin.runCatching {
+                val cart = fetchCartItemsKeyUseCase().getOrThrow()
+                listOf(
+                    BestBanchanModel.empty().copy(viewType = BestBanchanModel.ViewType.Banner)
+                ) + it.getOrThrow().map {
+                    it.banchans.map { banchan ->
+                        if(cart.contains(banchan.hash))
+                            banchan.copy(isCartItem = true)
+                        else
+                            banchan
+                    }
+                    it
                 }
-                it
             }
         }
     }
