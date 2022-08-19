bbdcce777d352f947408c09f57e429e698d684d1
wjchoi96 <wjchoi96@naver.com>
2022-08-20 01:51:29 +0900

    [Refactor] #115 CartListModelDiffUtilCallback 코드 스타일 수정

--- presentation/src/main/java/com/woowahan/banchan/CartListModelDiffUtilCallback.kt
+++ presentation/src/main/java/com/woowahan/banchan/CartListModelDiffUtilCallback.kt
@@ -14,47 +14,37 @@ class CartListModelDiffUtilCallback(
     override fun getNewListSize(): Int = newList.size
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        if(oldList[oldItemPosition] is CartListItemModel.Header && newList[newItemPosition] is CartListItemModel.Header){
-            Timber.d("header areItemsTheSame[$oldItemPosition][$newItemPosition] => ${oldList[oldItemPosition] == newList[newItemPosition]}")
-        }
         return oldList[oldItemPosition] isSameIdWith newList[newItemPosition]
-
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        if(oldList[oldItemPosition] is CartListItemModel.Header && newList[newItemPosition] is CartListItemModel.Header){
-            Timber.d("header areContentsTheSame[$oldItemPosition][$newItemPosition] => ${oldList[oldItemPosition] isSameIdWith  newList[newItemPosition]}")
-        }
         return oldList[oldItemPosition] isSameContentWith newList[newItemPosition]
     }
     
     override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
         val oldItem = oldList[oldItemPosition]
         val newItem = newList[newItemPosition]
-
-        if (oldItem is CartListItemModel.Header || newItem is CartListItemModel.Header) {
-            Timber.d("header getChangePayload debug[$oldItemPosition][$newItemPosition] => " +
-                    "${(oldItem as? CartListItemModel.Header)?.isAllSelected} " +
-                    "${(newItem as? CartListItemModel.Header)?.isAllSelected}")
-        }
-
-        if (oldItem is CartListItemModel.Header && newItem is CartListItemModel.Header) {
-            Timber.d("header getChangePayload => ${oldItem isSameIdWith newItem}")
-            if (!(oldItem isSameContentWith newItem)) {
-                return DefaultCartAdapter.Payload.SelectAllChanged
+        when {
+            oldItem is CartListItemModel.Header && newItem is CartListItemModel.Header -> {
+                if (!(oldItem isSameContentWith newItem)) {
+                    return DefaultCartAdapter.Payload.SelectAllChanged
+                }
             }
-        } else if (oldItem is CartListItemModel.Content && newItem is CartListItemModel.Content) {
-            if (oldItem.cart.isSelected != newItem.cart.isSelected) {
-                return DefaultCartAdapter.Payload.SelectOneChanged
+            oldItem is CartListItemModel.Content && newItem is CartListItemModel.Content -> {
+                if (oldItem.cart.isSelected != newItem.cart.isSelected) {
+                    return DefaultCartAdapter.Payload.SelectOneChanged
+                }
+                if (oldItem.cart.count != newItem.cart.count) {
+                    return DefaultCartAdapter.Payload.quantityChanged
+                }
             }
-            if (oldItem.cart.count != newItem.cart.count) {
-                return DefaultCartAdapter.Payload.quantityChanged
-            }
-        } else if (oldItem is CartListItemModel.Footer && newItem is CartListItemModel.Footer) {
-            if (!(oldItem isSameContentWith newItem)) {
-                return DefaultCartAdapter.Payload.totalPriceChanged
+            oldItem is CartListItemModel.Footer && newItem is CartListItemModel.Footer -> {
+                if (!(oldItem isSameContentWith newItem)) {
+                    return DefaultCartAdapter.Payload.totalPriceChanged
+                }
             }
         }
+
         return super.getChangePayload(oldItemPosition, newItemPosition)
     }
 }
