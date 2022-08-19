1fd96970061ca8b5dd5ed4d21029a88749dbd917
wjchoi96 <wjchoi96@naver.com>
2022-08-19 11:37:30 +0900

    [Fix] #115 CartlistModeDiffUtilCallback 수정
    - 의미에 맞게 비교 메소드 재배치

--- presentation/src/main/java/com/woowahan/banchan/CartListModelDiffUtilCallback.kt
+++ presentation/src/main/java/com/woowahan/banchan/CartListModelDiffUtilCallback.kt
@@ -3,6 +3,7 @@ package com.woowahan.banchan
 import androidx.recyclerview.widget.DiffUtil
 import com.woowahan.banchan.ui.adapter.DefaultCartAdapter
 import com.woowahan.domain.model.CartListItemModel
+import timber.log.Timber
 
 class CartListModelDiffUtilCallback(
     private val oldList: List<CartListItemModel>,
@@ -13,19 +14,33 @@ class CartListModelDiffUtilCallback(
     override fun getNewListSize(): Int = newList.size
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        if(oldList[oldItemPosition] is CartListItemModel.Header && newList[newItemPosition] is CartListItemModel.Header){
+            Timber.d("header areItemsTheSame[$oldItemPosition][$newItemPosition] => ${oldList[oldItemPosition] == newList[newItemPosition]}")
+        }
         return oldList[oldItemPosition] isSameIdWith newList[newItemPosition]
+
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldList[oldItemPosition] == newList[newItemPosition]
+        if(oldList[oldItemPosition] is CartListItemModel.Header && newList[newItemPosition] is CartListItemModel.Header){
+            Timber.d("header areContentsTheSame[$oldItemPosition][$newItemPosition] => ${oldList[oldItemPosition] isSameIdWith  newList[newItemPosition]}")
+        }
+        return oldList[oldItemPosition] isSameContentWith newList[newItemPosition]
     }
     
     override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
         val oldItem = oldList[oldItemPosition]
         val newItem = newList[newItemPosition]
 
+        if (oldItem is CartListItemModel.Header || newItem is CartListItemModel.Header) {
+            Timber.d("header getChangePayload debug[$oldItemPosition][$newItemPosition] => " +
+                    "${(oldItem as? CartListItemModel.Header)?.isAllSelected} " +
+                    "${(newItem as? CartListItemModel.Header)?.isAllSelected}")
+        }
+
         if (oldItem is CartListItemModel.Header && newItem is CartListItemModel.Header) {
-            if (!(oldItem isSameIdWith newItem)) {
+            Timber.d("header getChangePayload => ${oldItem isSameIdWith newItem}")
+            if (!(oldItem isSameContentWith newItem)) {
                 return DefaultCartAdapter.Payload.SelectAllChanged
             }
         } else if (oldItem is CartListItemModel.Content && newItem is CartListItemModel.Content) {
@@ -36,7 +51,7 @@ class CartListModelDiffUtilCallback(
                 return DefaultCartAdapter.Payload.quantityChanged
             }
         } else if (oldItem is CartListItemModel.Footer && newItem is CartListItemModel.Footer) {
-            if (!(oldItem isSameIdWith newItem)) {
+            if (!(oldItem isSameContentWith newItem)) {
                 return DefaultCartAdapter.Payload.totalPriceChanged
             }
         }
