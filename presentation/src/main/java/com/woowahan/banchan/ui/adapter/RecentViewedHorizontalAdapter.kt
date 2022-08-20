9145a291243772b678853d6f625bb5a9efb156ef
youngmu <tyg04308@naver.com>
2022-08-21 01:00:09 +0900

    [Add] #111 - ReceentViewedHorizontalAdapter

---
+++ presentation/src/main/java/com/woowahan/banchan/ui/adapter/RecentViewedHorizontalAdapter.kt
@@ -0,0 +1,75 @@
+package com.woowahan.banchan.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.woowahan.banchan.RecentViewedModelDiffUtilCallback
+import com.woowahan.banchan.databinding.ItemMenuBestHorizontalChildBinding
+import com.woowahan.banchan.databinding.ItemMenuSmallBinding
+import com.woowahan.domain.model.BanchanModel
+import com.woowahan.domain.model.RecentViewedItemModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import timber.log.Timber
+
+class RecentViewedHorizontalAdapter(
+    private val onItemCLick: (item: RecentViewedItemModel) -> Unit
+) :
+    RecyclerView.Adapter<RecentViewedHorizontalAdapter.RecentViewedHorizontalViewHolder>() {
+    private var banchanList = listOf<RecentViewedItemModel>()
+
+    fun updateList(newList: List<RecentViewedItemModel>) {
+        CoroutineScope(Dispatchers.Default).launch {
+            val diffCallback =
+                RecentViewedModelDiffUtilCallback(banchanList, newList, "")
+            val diffRes = DiffUtil.calculateDiff(diffCallback)
+            withContext(Dispatchers.Main) {
+                banchanList = newList.toList()
+                diffRes.dispatchUpdatesTo(this@RecentViewedHorizontalAdapter)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecentViewedHorizontalViewHolder {
+        return RecentViewedHorizontalViewHolder.from(
+            parent,
+            onItemCLick
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecentViewedHorizontalViewHolder, position: Int) {
+        holder.bind(banchanList[position])
+    }
+
+    override fun getItemCount(): Int = banchanList.size
+
+    class RecentViewedHorizontalViewHolder(
+        private val binding: ItemMenuSmallBinding,
+        val itemClickListener: (RecentViewedItemModel) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        companion object {
+            fun from(
+                parent: ViewGroup,
+                itemClickListener: (RecentViewedItemModel) -> Unit
+            ): RecentViewedHorizontalViewHolder =
+                RecentViewedHorizontalViewHolder(
+                    ItemMenuSmallBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    ),
+                    itemClickListener
+                )
+        }
+
+        fun bind(item: RecentViewedItemModel) {
+            binding.banchan = item
+        }
+    }
+}
