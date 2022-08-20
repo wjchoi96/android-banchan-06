58b10f30dcb1d94f3cbeb30acf7c715348c1fe03
wjchoi96 <wjchoi96@naver.com>
2022-08-20 16:48:29 +0900

    [Refactor] #112 Cart UseCase 수정
    - Repository가 Flow를 리턴하는것에 대한 적용
    - UseCase가 Flow<DomainEvent<T>>를 리턴하도록 수정

--- domain/src/main/java/com/woowahan/domain/usecase/cart/UpdateCartItemSelectUseCase.kt
+++ domain/src/main/java/com/woowahan/domain/usecase/cart/UpdateCartItemSelectUseCase.kt
@@ -1,13 +1,20 @@
 package com.woowahan.domain.usecase.cart
 
+import com.woowahan.domain.model.DomainEvent
 import com.woowahan.domain.repository.CartRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
 
 class UpdateCartItemSelectUseCase(
     private val cartRepository: CartRepository
 ) {
-    suspend operator fun invoke(isSelect: Boolean, vararg hashes: String): Result<Boolean> {
-        return kotlin.runCatching {
-            cartRepository.updateCartItemSelect(isSelect, *hashes).getOrThrow()
-        }
+    suspend operator fun invoke(isSelect: Boolean, vararg hashes: String): Flow<DomainEvent<Boolean>> = flow<DomainEvent<Boolean>> {
+        cartRepository.updateCartItemSelect(isSelect, *hashes)
+            .collect {
+                emit(DomainEvent.success(it))
+            }
+    }.catch {
+        emit(DomainEvent.failure(it))
     }
 }
