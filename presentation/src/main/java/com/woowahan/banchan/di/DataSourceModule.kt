d00d6f65b221d3930b904732656f17632ed2ca8a
wjchoi96 <wjchoi96@naver.com>
2022-08-22 19:19:45 +0900

    [Modify] #121 Di Module들의 Scope를 Singleton으로 통일
    - 일단은, WorkManager에 종속성 주입을 위한 수정
    - Module의 Scope와 내부에 provide로 주입되고있는 인스턴스들의 scope가 실제로 일치하지 않는다는 사실을 인지하게 되어서, 실제 singleton으로 생성되는것 외에는 none-scope 모드로 처리

--- presentation/src/main/java/com/woowahan/banchan/di/DataSourceModule.kt
+++ presentation/src/main/java/com/woowahan/banchan/di/DataSourceModule.kt
@@ -4,10 +4,10 @@ import com.woowahan.data.datasource.*
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 abstract class DataSourceModule {
 
     @Binds
