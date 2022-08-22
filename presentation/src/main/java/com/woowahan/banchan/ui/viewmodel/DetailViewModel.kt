0c7e5eb4ff7b7e59a28a5d016ffb2a3f9000ffeb
youngmu <tyg04308@naver.com>
2022-08-23 01:12:40 +0900

    [Add] #137 - Init detail activity

---
+++ presentation/src/main/java/com/woowahan/banchan/ui/viewmodel/DetailViewModel.kt
@@ -0,0 +1,8 @@
+package com.woowahan.banchan.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class DetailViewModel: ViewModel() {
+}
