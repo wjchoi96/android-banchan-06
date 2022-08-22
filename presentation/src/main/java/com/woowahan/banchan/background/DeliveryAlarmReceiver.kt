7229825a74b824be4af2e560d209256fea34ab2d
wjchoi96 <wjchoi96@naver.com>
2022-08-22 19:13:48 +0900

    [Feature] #121 Delivery AlarmReceiver 작성
    - 정확한 시간에 호출되어야 하는 작업을 Alarm Manager로 예약
    - 예약된 작업을 실행할 Receiver 작성

--- presentation/src/main/java/com/woowahan/banchan/background/DeliveryAlarmReceiver.kt
+++ presentation/src/main/java/com/woowahan/banchan/background/DeliveryAlarmReceiver.kt
@@ -3,7 +3,7 @@ package com.woowahan.banchan.background
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.woowahan.banchan.util.NotificationUtil
+import androidx.work.WorkManager
 import timber.log.Timber
 
 class DeliveryAlarmReceiver: BroadcastReceiver() {
@@ -20,17 +20,18 @@ class DeliveryAlarmReceiver: BroadcastReceiver() {
         }
     }
     override fun onReceive(p0: Context?, p1: Intent?) {
-        Timber.d("DeliveryAlarmReceiver onReceive => ###############")
+        Timber.d("Delivery background debug => DeliveryAlarmReceiver onReceive => ###############")
         if(p0 == null) return
         p1?.let {
             val orderId = it.getLongExtra(EXTRA_ORDER_ID, 0)
             val orderTitle = it.getStringExtra(EXTRA_ORDER_TITLE)
-            Timber.d("orderId[$orderId], orderTitle[$orderTitle]")
-            NotificationUtil.createNotification(
-                p0,
-                orderTitle,
-                orderId
-            )
+            setDeliveryWorker(p0, orderId, orderTitle)
         }
+    }
+
+    private fun setDeliveryWorker(context: Context, orderId: Long, orderTitle: String?){
+        val deliveryWorkRequest = DeliveryWorker.getRequest(orderId, orderTitle)
+        WorkManager.getInstance(context.applicationContext)
+            .enqueue(deliveryWorkRequest)
     }
 }
