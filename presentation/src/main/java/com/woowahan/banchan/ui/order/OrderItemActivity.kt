a6a828b511d370d2fa218f5c372b00adf3a8f155
wjchoi96 <wjchoi96@naver.com>
2022-08-24 21:49:18 +0900

    [Fix] #152 OrderItemViewModel 수정
    - Room과 연결된 Flow가 중복되어 collect 하지 않도록 init 에서 flow를 연결
    - refresh도 새로운 파이프를 연결하던 방식에서, time만 업데이트 할 수 있도록 유도하는 방식으로 변경

--- presentation/src/main/java/com/woowahan/banchan/ui/order/OrderItemActivity.kt
+++ presentation/src/main/java/com/woowahan/banchan/ui/order/OrderItemActivity.kt
@@ -129,8 +129,6 @@ class OrderItemActivity : BaseActivity<ActivityOrderItemBinding>() {
                     adapter.updateList(it)
                 }
             }
-
-            viewModel.fetchOrder()
         }
     }
 }
--- presentation/src/main/java/com/woowahan/banchan/ui/viewmodel/OrderItemViewModel.kt
+++ presentation/src/main/java/com/woowahan/banchan/ui/viewmodel/OrderItemViewModel.kt
@@ -2,17 +2,16 @@ package com.woowahan.banchan.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.woowahan.domain.model.DomainEvent
 import com.woowahan.domain.model.OrderItemTypeModel
 import com.woowahan.domain.usecase.order.FetchOrderUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
-import timber.log.Timber
+import java.util.*
 import javax.inject.Inject
 
-@OptIn(FlowPreview::class)
 @HiltViewModel
 class OrderItemViewModel @Inject constructor(
     private val fetchOrderUseCase: FetchOrderUseCase
@@ -20,13 +19,13 @@ class OrderItemViewModel @Inject constructor(
     private val _dataLoading: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val dataLoading = _dataLoading.asStateFlow()
 
-    private val _orderItem: MutableStateFlow<List<OrderItemTypeModel>> = MutableStateFlow(emptyList())
-    val orderItem = _orderItem.asStateFlow()
+    private val _orderItem = fetchOrder()
+    val orderItem: SharedFlow<List<OrderItemTypeModel>>
 
     private val _eventFlow: MutableSharedFlow<UiEvent> = MutableSharedFlow()
     val eventFlow = _eventFlow.asSharedFlow()
 
-    private val _refreshFlow: MutableSharedFlow<Unit> = MutableSharedFlow()
+    private val _refreshEvent: MutableSharedFlow<Boolean> = MutableSharedFlow(replay = 1)
 
     private var orderId: Long? = null
     fun initData(orderId: Long){
@@ -34,45 +33,47 @@ class OrderItemViewModel @Inject constructor(
     }
 
     init {
-        viewModelScope.launch {
-            _refreshFlow
-                .debounce(300)
-                .collect {
-                    Timber.d("refreshFlow on")
-                    fetchOrder()
+        orderItem = combine(_refreshEvent, _orderItem) { _, event ->
+            var orderList: List<OrderItemTypeModel> = emptyList()
+            event.onSuccess {
+                orderList = it.toMutableList().apply {
+                    this[0] = (this[0] as OrderItemTypeModel.Header).copy(currentDate =  Calendar.getInstance().time)
                 }
-        }
+            }.onFailureWithData{ it, data ->
+                it.printStackTrace()
+                it.message?.let { _eventFlow.emit(UiEvent.ShowToast(it)) }
+                if(data != null)
+                    orderList = data
+                else
+                    _eventFlow.emit(UiEvent.FinishView(it.message))
+            }.also {
+                _dataLoading.value = false
+            }
+            return@combine orderList
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
+        viewModelScope.launch { _refreshEvent.emit(true) }
     }
 
-    fun fetchOrder(){
-        viewModelScope.launch {
-            if(orderId == -1L){
-                _eventFlow.emit(UiEvent.FinishView("잘못된 주문번호입니다"))
-                return@launch
-            }
-            _dataLoading.value = true
-            fetchOrderUseCase(orderId!!)
-                .flowOn(Dispatchers.Default)
-                .collect { event ->
-                    event.onSuccess {
-                        Timber.d("get order success -> $it")
-                        _orderItem.value = it
-                    }.onFailureWithData{ it, data ->
-                        it.printStackTrace()
-                        it.message?.let {
-                            _eventFlow.emit(UiEvent.ShowToast(it))
-                        }
-                        data?.let { _orderItem.value = it }
-                    }.also {
-                        _dataLoading.value = false
-                    }
-                }
+    private fun fetchOrder(): Flow<DomainEvent<List<OrderItemTypeModel>>> = flow {
+        if(orderId == -1L){
+            _eventFlow.emit(UiEvent.FinishView("잘못된 주문번호입니다"))
+            return@flow
         }
+        _dataLoading.value = true
+        fetchOrderUseCase(orderId!!)
+            .flowOn(Dispatchers.Default)
+            .collect {
+                emit(it)
+            }
     }
 
     fun refreshEvent(){
         viewModelScope.launch {
-            _refreshFlow.emit(Unit)
+            _refreshEvent.emit(true)
         }
     }
 
