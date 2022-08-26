3539f1335d4c2e6bbe021864680903fb70da3409
wjchoi96 <wjchoi96@naver.com>
2022-08-27 03:21:11 +0900

    [Refactor] #203 CartRepositoryImpl 에러 처리 수정
    - async, await 를 호출했을때 에러가 발생했다면, JobCancellationException 에러가 발생
    - JobCancellationException 은 try catch 가 없다면 로그도 찍히지 않고 조용히 처리되는 에러인데다가, 어떤 원인으로 Cancel 이 되었는지도 알려주지 않기 때문에, 원인을 알수가 없다
    - 그래서, async 내부에서 발생한 에러를 캡쳐해준뒤, await 이후 직접 throw 해줘서 외부로 에러를 전달해주도록 설정

--- data/src/main/java/com/woowahan/data/repository/CartRepositoryImpl.kt
+++ data/src/main/java/com/woowahan/data/repository/CartRepositoryImpl.kt
@@ -3,7 +3,6 @@ package com.woowahan.data.repository
 import com.woowahan.data.datasource.BanchanDetailCacheDataSource
 import com.woowahan.data.datasource.BanchanDetailDataSource
 import com.woowahan.data.datasource.CartDataSource
-import com.woowahan.data.entity.BanchanDetailEntity
 import com.woowahan.domain.extension.priceStrToLong
 import com.woowahan.domain.model.CartModel
 import com.woowahan.domain.repository.CartRepository
@@ -12,7 +11,7 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
@@ -72,6 +71,7 @@ class CartRepositoryImpl @Inject constructor(
     }.flowOn(coroutineDispatcher)
 
     override suspend fun fetchCartItems(): Flow<List<CartModel>> = flow<List<CartModel>> {
+        var throwable: Throwable? = null
         cartDataSource.fetchCartItems()
             .collect { list ->
                 coroutineScope {
@@ -82,28 +82,35 @@ class CartRepositoryImpl @Inject constructor(
                                 else -> {
                                     println("fetchCartItems async run => ${it.hash}")
                                     banchanDetailDataSource.fetchBanchanDetail(it.hash)
-                                        .firstOrNull()?.also {
+                                        .catch {
+                                            throwable = it
+                                            throw it
+                                        }.collect {
                                             banchanDetailCacheDataSource.saveItem(it)
-                                    }
+                                        }
                                 }
                             }
                         }
-                    }.awaitAll()
-
-                    val res = list.map {
-                        banchanDetailCacheDataSource.getItem(it.hash).run {
-                            CartModel(
-                                hash = it.hash,
-                                count = it.count,
-                                title = it.title,
-                                imageUrl = this.data.thumbImages.first(),
-                                price = this.data.prices.last().priceStrToLong(),
-                                isSelected = it.isSelect
-                            )
-                        }
+                    }.run {
+                        awaitAll() // JobCancellationException 이 발생하면 어떤 에러로 인해 Cancel 되었는지 모르게 조용히 처리된다, try catch 가 없으면 로그에 찍히지도 않는다
                     }
-                    emit(res)
                 }
+                if(throwable != null) // 때문에, 에러가 발생하면 해당 에러가 무엇인지 캡쳐해 두었다가, 해당 에러를 직접 throw
+                    throw throwable!!
+
+                val res = list.map {
+                    banchanDetailCacheDataSource.getItem(it.hash).run {
+                        CartModel(
+                            hash = it.hash,
+                            count = it.count,
+                            title = it.title,
+                            imageUrl = this.data.thumbImages.first(),
+                            price = this.data.prices.last().priceStrToLong(),
+                            isSelected = it.isSelect
+                        )
+                    }
+                }
+                emit(res)
             }
     }.flowOn(coroutineDispatcher)
 
